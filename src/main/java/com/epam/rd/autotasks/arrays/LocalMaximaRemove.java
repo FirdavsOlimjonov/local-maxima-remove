71723595e51e6e81468d8c68c508da92a97ce66f
firdavs <firdavsolimjonov25@gmail.com>
2022-12-13 11:12:46 +0500

    added

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -1,6 +1,8 @@
 package com.epam.rd.autotasks.arrays;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class LocalMaximaRemove {
 
@@ -10,10 +12,24 @@ public class LocalMaximaRemove {
         System.out.println(Arrays.toString(removeLocalMaxima(array)));
     }
 
-    public static int[] removeLocalMaxima(int[] array){
+    public static int[] removeLocalMaxima(int[] array) {
+        List<Integer> list = new LinkedList<>();
 
-        //put your code here
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0 && (array[i + 1] < array[i])) list.add(array[i]);
+            if (i == array.length - 1 && array[i - 1] < array[i]) list.add(array[i]);
+            if (i > 0 && array[i + 1] < array[i] && array[i - 1] < array[i]) list.add(array[i]);
+        }
 
-        throw new UnsupportedOperationException();
+        if (list.size() == array.length || list.size() == 0)
+            return Arrays.copyOf(array, array.length);
+
+        int[] res = new int[list.size()];
+        int index = 0;
+
+        for (Integer integer : list)
+            res[index++] = integer;
+
+        return res;
     }
 }
